d58b6faf04ab437b62d4943965aa31f32f9a1f12
Arthurbiasibetti <arthurbiasibettifarias@gmail.com>
2022-10-30 15:48:56 -0300

    minor fix

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -5,11 +5,6 @@ import tsoaSwagger from '../routes/swagger.json';
 function swaggerDocs(app: Express, url: string, port: number) {
   app.use('/docs', swaggerUi.serve, swaggerUi.setup(tsoaSwagger));
 
-  // app.get('/docs.json', (req: Request, res: Response) => {
-  //   res.setHeader('Content-Type', 'application/json');
-  //   res.send(tsoaSwagger);
-  // });
-
   console.log(`Documentação disponível em http://${url}:${port}/docs`);
 }
 
